f696dc3684c6ecbcd65964a878b83fca662766c4
Ryan Hedges <ryanhedges@gmail.com>
2015-01-01 23:20:44 -0800

    Make happy and sad path tests pass. This is basic
    I'm still in the basic stage of solving this problem. The logic isn't
    complicated and it isn't very useful.

--- Minitest/bracer_validator/bracer_validator.rb
+++ Minitest/bracer_validator/bracer_validator.rb
@@ -1,5 +1,9 @@
 class BracerValidator
   def validate(code_string)
-    true
+    if code_string == "{}"
+      return true
+    else
+      return false
+    end
   end
 end
--- Minitest/bracer_validator/bracer_validator_test.rb
+++ Minitest/bracer_validator/bracer_validator_test.rb
@@ -5,8 +5,12 @@ describe BracerValidator do
   describe "integration/big picture tests" do
     it "returns true when the bracers are valid" do
       bracer_validator = BracerValidator.new
-
       bracer_validator.validate("{}").must_equal true
+    end
+
+    it "returns false when the bracers are invalid" do
+      bracer_validator = BracerValidator.new
+      bracer_validator.validate("[}").must_equal false
     end
   end
 end
