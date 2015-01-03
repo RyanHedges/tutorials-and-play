11c7310781970b733b9954bb3f30debc20424124
Ryan Hedges <ryanhedges@gmail.com>
2015-01-03 01:23:29 -0800

    Write next failing tests
    I struggle to understand how to use MiniTest to find a way to do BDD. It seems
    like I have state based testing available and that's it. I added another state
    based test hoping it would guide me closer to discovering how I want to solve
    this problem.

--- Minitest/bracer_validator/bracer_validator_test.rb
+++ Minitest/bracer_validator/bracer_validator_test.rb
@@ -3,14 +3,20 @@ require_relative "bracer_validator"
 
 describe BracerValidator do
   describe "integration/big picture tests" do
+    before do
+      @bracer_validator = BracerValidator.new
+    end
+
     it "returns true when the bracers are valid" do
-      bracer_validator = BracerValidator.new
-      bracer_validator.validate("{}").must_equal true
+      @bracer_validator.validate("{}").must_equal true
     end
 
     it "returns false when the bracers are invalid" do
-      bracer_validator = BracerValidator.new
-      bracer_validator.validate("[}").must_equal false
+      @bracer_validator.validate("[}").must_equal false
+    end
+
+    it "returns true when the multiple bracers are properly nested" do
+      @bracer_validator.validate("()").must_equal true
     end
   end
 end
